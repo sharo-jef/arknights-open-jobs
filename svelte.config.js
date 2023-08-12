830142ed77ef75dbd6d8d496cbd0c1c896bde8df
sharo-jef <sharo.jef@gmail.com>
2023-08-12 21:02:50 +0900

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const production = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -19,6 +21,9 @@ const config = {
 			precompress: false,
 			strict: true,
 		}),
+		paths: {
+			base: production ? '/arknights-tools' : '',
+		},
 		// outDir: 'docs',
 	},
 };
