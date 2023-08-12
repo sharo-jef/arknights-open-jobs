e3c0ab33a6833d640f16152d97156eba84c98143
sharo-jef <sharo.jef@gmail.com>
2023-08-12 20:54:05 +0900

    update svelte.config.js

--- docs/output/server/entries/pages/public-recruitment/_page.ts.js
+++
@@ -1,5 +0,0 @@
-import "csv-parse/browser/esm/sync";
-const load = async ({ fetch }) => ({ data: [] });
-export {
-  load
-};
--- docs/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.169bfde8.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d545fc7a.js","_app/immutable/chunks/singletons.3340d1b2.js"];
-export const stylesheets = [];
-export const fonts = [];
