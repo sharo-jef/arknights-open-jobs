2f62e4c3ab1449a74f8e0a3a592533ab052e68c9
sharo-jef <sharo.jef@gmail.com>
2023-08-12 20:47:36 +0900

    update package.json

--- docs/output/server/manifest-full.js
+++ docs/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["date.txt","favicon.png","open-jobs.csv"]),
 	mimeTypes: {".txt":"text/plain",".png":"image/png",".csv":"text/csv"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.e2ccdb1d.js","app":"_app/immutable/entry/app.b5e2a9b6.js","imports":["_app/immutable/entry/start.e2ccdb1d.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.47426bdd.js","_app/immutable/entry/app.b5e2a9b6.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d545fc7a.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.debc1c13.js","app":"_app/immutable/entry/app.84f39598.js","imports":["_app/immutable/entry/start.debc1c13.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.3340d1b2.js","_app/immutable/entry/app.84f39598.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.d545fc7a.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
